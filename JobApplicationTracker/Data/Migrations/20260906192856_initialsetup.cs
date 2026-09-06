e31a58d4acf23c0c99d9e893ef12bb5d5b138507
alexzadams-TechDevelopment <alexzadamswork@gmail.com>
2026-09-06 22:45:48 +0100

    Commit 3: Created an inital setup controller to set up a Object Relation Mapper
    Jobs Index can now presents premade SQL using ORM to make a simplified database to hold job application data. (using items from the Job Model).

---
+++ JobApplicationTracker/Data/Migrations/20260906192856_initialsetup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobApplicationTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialsetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Job",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    JobName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobStatus = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Job", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Job");
+        }
+    }
+}
